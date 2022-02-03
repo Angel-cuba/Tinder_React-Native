0cc20456e127a8231d59af6eae2f0afe9af131f3
Mac_Developer <araozangel842@gmail.com>
2022-02-03 11:56:53 +0200

    Starting with React-Native-Cli

---
+++ App.js
@@ -0,0 +1,20 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={{color: 'white'}}>Hola</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
